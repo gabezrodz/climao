367c06b26538faf3c438a39216d7c46dec2a618d
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-03-04 02:12:34 -0300

    Feat: adicionado api via axios

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://api.openweathermap.org/data/2.5",
+  params: {
+    lang: "Portuguese - pt",
+    units: "metric",
+    q: null,
+    appid: "b1398e81950c8225db4896322b47cf6f",
+  }
+});
+
+export default API;
